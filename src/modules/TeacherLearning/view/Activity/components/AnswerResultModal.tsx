9c12794cda3c8f1440b459ba823ebdcf04028e40
saurav.sapariya <saurav.sapariya@agileinfoways.com>
2026-01-27 12:16:05 +0530

    worked on feedback changes

--- src/modules/TeacherLearning/view/Activity/components/AnswerResultModal.tsx
+++ src/modules/TeacherLearning/view/Activity/components/AnswerResultModal.tsx
@@ -52,7 +52,7 @@ export const AnswerResultModal: React.FC<AnswerResultModalProps> = ({
 
     return (
       <>
-        {/* <p>You have given incorrect answer. You may move to the next question.</p> */}
+        <p>You have given incorrect answer. You may move to the next question.</p>
         <AnswerResultModalContent>
           <RightAnswerLabel>Right Answer:</RightAnswerLabel>
           <CorrectAnswerBox>{correctAnswer?.option_text}</CorrectAnswerBox>
